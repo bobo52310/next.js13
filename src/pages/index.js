2b6380a1bc0cde9469bedac578eb3bb14184339d
Bobo Chen <bobo52310@gmail.com>
2023-01-29 18:03:14 +0800

    Change created by Vercel from to Bobo.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -26,15 +26,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
+              By Bobo{" "}
             </a>
           </div>
         </div>
@@ -119,5 +111,5 @@ export default function Home() {
         </div>
       </main>
     </>
-  )
+  );
 }
